baf6b75da43c50c2ec113bcfc3750aeef0b99836
Sarthak Niranjan <22b3923@iitb.ac.in>
2023-12-14 16:05:09 +0530

    test

--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -1,8 +1,19 @@
 const express = require('express');
+//importing express router
 const router = express.Router();
+//importing user module
+const User = require('../models/User');
+//importing express validator, to add data validation
+const {body, validationResult} = require('express-validator');
 
-router.get('/', (req, res)=>{
+//create a user using: POST "/api/auth". Doesn't require auth
+router.post('/', (req, res)=>{
+    console.log(req.body);
     res.send(req.body);
+    //creating a new user, using the request body
+    const user = User(req.body);
+    //saving the new user
+    user.save();
 });
 
 module.exports = router;
