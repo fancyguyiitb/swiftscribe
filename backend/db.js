c01d4b33d83d682881e838787d273489cc86a661
Sarthak Niranjan <22b3923@iitb.ac.in>
2024-01-08 22:37:52 +0530

    folder restructuring

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const mongoURI = 'mongodb://127.0.0.1:27017/swiftscribe';
+// const mongoURI = 'mongodb+srv://technicalpurush:juarez2004@cluster0.uiuvaeb.mongodb.net/?retryWrites=true&w=majority';
 
 const connectToMongo = ()=>{
     mongoose.connect(mongoURI).then(
